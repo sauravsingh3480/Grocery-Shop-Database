1f64f313c6b5f27f125824c4d58370d17818ca49
SAURAV KUMAR <164927556+sauravsingh3480@users.noreply.github.com>
2024-10-06 01:33:32 +0530

    Adding Complete Grocery Shop Database

---
+++ GroceryCategory.sql
@@ -0,0 +1,9 @@
+create table Category (
+	id INT,
+	name VARCHAR(2)
+);
+insert into Category (id, name) values (1, 'c1');
+insert into Category (id, name) values (2, 'c2');
+insert into Category (id, name) values (3, 'c3');
+insert into Category (id, name) values (4, 'c4');
+insert into Category (id, name) values (5, 'c5');
---
+++ ItemTags.sql
@@ -0,0 +1,19 @@
+create table Tag (
+	id INT,
+	name VARCHAR(5)
+);
+insert into Tag (id, name) values (1, 'tag1');
+insert into Tag (id, name) values (2, 'tag2');
+insert into Tag (id, name) values (3, 'tag3');
+insert into Tag (id, name) values (4, 'tag4');
+insert into Tag (id, name) values (5, 'tag5');
+insert into Tag (id, name) values (6, 'tag6');
+insert into Tag (id, name) values (7, 'tag7');
+insert into Tag (id, name) values (8, 'tag8');
+insert into Tag (id, name) values (9, 'tag9');
+insert into Tag (id, name) values (10, 'tag10');
+insert into Tag (id, name) values (11, 'tag11');
+insert into Tag (id, name) values (12, 'tag12');
+insert into Tag (id, name) values (13, 'tag13');
+insert into Tag (id, name) values (14, 'tag14');
+insert into Tag (id, name) values (15, 'tag15');
